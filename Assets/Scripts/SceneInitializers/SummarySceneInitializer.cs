561cb3ebc3af3904c81e7bfba0dd7116ee21f8fd
AmitReuveni <amitreuveni@gmail.com>
2023-07-25 12:02:47 +0300

    added summary scene initializer that assigns correct behavior to main menu button on the scene

---
+++ Assets/Scripts/SceneInitializers/SummarySceneInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SceneInitializers
+{
+    public class SummarySceneInitializer : MonoBehaviour
+    {
+        [SerializeField] private Button mainMenuButton;
+        [SerializeField]
+
+        private void Start()
+        {
+            mainMenuButton.onClick.AddListener(LoadManager.Instance.LoadMainMenuScene);
+        }
+    }
+}
+
